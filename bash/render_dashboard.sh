9153d58921c48bc382c6fc50bf24515d1c544311
RamiKrispin <ramkrisp@umich.edu>
2022-09-17 15:53:54 -0700

    update the bash script arguments

--- bash/render_dashboard.sh
+++ bash/render_dashboard.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 echo "Rendering the dashboard..."
+if [[ "$1" = ""  || "$2" = "" ]] ; then
+    echo "The git user.name and/or user.email are missing"
+    exit 0
+else
+    echo "Git user.name is $1"
+    echo "Git user.email is $2"
+fi
+
 
 Rscript -e "rmarkdown::render_site()"
 
 if [[ "$(git status --porcelain)" != "" ]]; then
-    git config --global user.name 'RamiKrispin'
-    git config --global user.email 'ramkrisp@umich.edu'
+    git config --global user.name $1
+    git config --global user.email $2
     git add *
     git commit -m "Auto update dashboard"
     git push
