b0a115cbc0b5ccf6edd3da6408a9fdd02d5161af
RamiKrispin <ramkrisp@umich.edu>
2022-09-17 17:57:48 -0700

    testing the git commit on GA

--- bash/render_dashboard.sh
+++ bash/render_dashboard.sh
@@ -12,10 +12,3 @@ fi
 
 Rscript -e "rmarkdown::render_site()"
 
-if [[ "$(git status --porcelain)" != "" ]]; then
-    git config --global user.name "RamiKrispin"
-    git config --global user.email "ramkrisp@umich.edu"
-    git add *
-    git commit -m "Auto update dashboard"
-    git push
-fi
