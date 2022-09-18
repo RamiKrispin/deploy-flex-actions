39cae14ed59f7877245f394cd76922b4679afa93
Rami Krispin <12760966+RamiKrispin@users.noreply.github.com>
2022-09-17 18:07:24 -0700

    Update render_dashboard.sh

--- bash/render_dashboard.sh
+++ bash/render_dashboard.sh
@@ -13,8 +13,8 @@ fi
 Rscript -e "rmarkdown::render_site()"
 
 if [[ "$(git status --porcelain)" != "" ]]; then
-    git config --global user.name $1
-    git config --global user.email $2
+    git config --global user.name 'RamiKrispin'
+    git config --global user.email 'ramkrisp@umich.edu'
     git add *
     git commit -m "Auto update dashboard"
     git push
