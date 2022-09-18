0c06d8cf98d7d4694b6f1bb67b298944ca8fa38d
Rami Krispin <12760966+RamiKrispin@users.noreply.github.com>
2022-09-17 18:11:55 -0700

    Update render_dashboard.sh

--- bash/render_dashboard.sh
+++ bash/render_dashboard.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 echo "Rendering the dashboard..."
-if [[ "$1" = ""  || "$2" = "" ]] ; then
-    echo "The git user.name and/or user.email are missing"
-    exit 0
-else
-    echo "Git user.name is $1"
-    echo "Git user.email is $2"
-fi
-
 
 Rscript -e "rmarkdown::render_site()"
 
