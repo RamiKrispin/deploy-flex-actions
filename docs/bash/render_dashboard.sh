99bd502c863feaead235706300ed9bd2cf8858c6
RamiKrispin <ramkrisp@umich.edu>
2022-09-17 19:49:16 -0700

    update the dashboard

--- docs/bash/render_dashboard.sh
+++ docs/bash/render_dashboard.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 echo "Rendering the dashboard..."
-if [[ "$1" = ""  || "$2" = "" ]] ; then
-    echo "The git user.name and/or user.email are missing"
-    exit 0
-else
-    echo "Git user.name is $1"
-    echo "Git user.email is $2"
-fi
-
 
 Rscript -e "rmarkdown::render_site()"
 
@@ -16,7 +8,6 @@ Rscript -e "rmarkdown::render_site()"
 git config --global --add safe.directory /__w/deploy-flex-actions/deploy-flex-actions
 
 if [[ "$(git status --porcelain)" != "" ]]; then
-    
     git config --global user.name $1
     git config --global user.email $2
     git add *
